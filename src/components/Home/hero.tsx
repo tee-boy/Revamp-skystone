1b98506728c00f4d74d060590699555f338eae71
tee-boy <tochukwuemmanuel690@gmail.com>
2025-08-01 11:38:08 +0100

    Comit Changed updates

---
+++ src/components/Home/hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+        <h1 className="text-4xl font-bold">Welcome to Our Website</h1>
+        <p className="mt-4 text-lg">We are glad to have you here. Explore our content and enjoy your stay!</p>
+    </div>
+  )
+}
+
+export default Hero
